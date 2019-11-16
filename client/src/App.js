a1cb50bbf10f862f13f6a54b4b529512f22de9ec
Ronny Moura <ronimour@gmail.com>
2019-11-16 18:28:46 -0300

    Fixed App and tests

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,18 @@ import Fib from './Fib'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title" >Welcome to Fibonacci Calculator</h1>
+          <Link to="/" >Home</Link>
+          <Link to="/otherpage" >Other Page</Link>
+        </header> 
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />          
+      </div>
+    </Router>
   );
 }
 
